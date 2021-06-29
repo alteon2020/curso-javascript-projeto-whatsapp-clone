24bfc441f613bbd5fb2c7326a87bd1b9828c6d6a
alteon2020 <alteonalves@gmail.com>
2021-06-28 21:57:21 -0300

    aula02

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,18 +1,104 @@
 class WhatsAppController {
     constructor() {
         console.log('WhatsAppController OK');
-
+        this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
-/*
- * Pega todos os ids da página e chama a função pra transformar em camelCase
- */
+    /*
+     * Pega todos os ids da página e chama a função pra transformar em camelCase
+     */
 
     loadElements() {
         this.el = {};
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element;
         });
+    }
+
+    /*
+     * Aplica prototipos para facilitar a manipulação do DOM  
+    */
+
+    elementsPrototype() {
+        Element.prototype.hide = function () {
+            this.style.display = 'none';
+            return this;
+        }
+        Element.prototype.show = function () {
+            this.style.display = 'block';
+            return this;
+        }
+        Element.prototype.toggle = function () {
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+        Element.prototype.on = function (events, fn) {
+            events.split(' ').forEach(event => {
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+        Element.prototype.css = function (styles) {
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+        Element.prototype.addClass = function (name) {
+            this.classList.add(name);
+            return this;
+        }
+        Element.prototype.removeClass = function (name) {
+            this.classList.remove(name);
+        }
+        Element.prototype.toggleClass = function (name) {
+            this.classList.toggle(name);
+            return this;
+        }
+        Element.prototype.hasClass = function (name) {
+            return this.classList.contains(name);
+        }
+
+    }
+    /* 
+     * Metódo que inicia todos os eventos
+     *
+     */
+    initEvents() {
+        //Abre o painel de edição de foto
+        this.el.myPhoto.on('click', e => {
+            
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+
+        });
+        //Fecha o painel de adição de foto
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+        //Abre o painel de novo contato
+        this.el.btnNewContact.on('click', e => {
+            
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+
+        });
+        //Fecha o painel de novo contato
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        });
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 }
 
--- src/util/Format.js
+++ src/util/Format.js
@@ -2,7 +2,7 @@ class Format {
     /*
      * Retorna os id's dos elementos transformados em CamelCase
     */
-    static getCamelCase(text){
+    static getCamelCase(text) {
         let div = document.createElement('div');
         div.innerHTML = `<div data-${text}="id"></div>`;
         return Object.keys(div.firstChild.dataset)[0];
