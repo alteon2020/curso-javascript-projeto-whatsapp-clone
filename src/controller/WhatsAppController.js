a988ebcf7f4b043184e3e777ca885d8f2aefcb22
alteon2020 <alteonalves@gmail.com>
2021-06-29 20:17:43 -0300

    Aula03

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -118,20 +118,169 @@ class WhatsAppController {
 
         //Evento para anexar foto
         this.el.btnAttachPhoto.on('click', e => {
-            console.log('photo');
+            this.el.inputPhoto.click();
         });
+
+        this.el.inputPhoto.on('change', e => {
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            })
+        });
+
         //Evento para anexar camera
+        //Abre o painel de tirar foto
         this.el.btnAttachCamera.on('click', e => {
-            console.log('Camera');
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)',
+            });
         });
-        //Evento para anexar foto
+
+        //Fecha o painel de tirar foto
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        // Evento para tirar foto 
+        this.el.btnTakePicture.on('click', e => {
+            console.log('Take Picture');
+        });
+
+        //Evento para anexar documento
         this.el.btnAttachDocument.on('click', e => {
-            console.log('Document');
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)',
+            });
         });
-        //Evento para anexar foto
+
+        //fecha o painel de documento
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        //Botão que envia o documento
+        this.el.btnSendDocument.on('click', e => {
+            console.log("Send document");
+        });
+
+        //Evento para anexar contato
         this.el.btnAttachContact.on('click', e => {
-            console.log('contact');
+            this.el.modalContacts.show();
         });
+
+        //Fecha o modal de contato
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+
+        //Evento de microfone
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        });
+
+        //Fecha o microphone
+        this.el.btnCancelMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+        //Envia o audio do microphone
+        this.el.btnFinishMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+        // Campo de digitação
+        this.el.inputText.on('keypress', e => {
+            if (e.key === 'Enter' && !e.ctrlKey) {
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+        });
+
+        this.el.inputText.on('keyup', e => {
+            if (this.el.inputText.innerHTML.length) {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+        });
+
+        //Envia mensagem
+        this.el.btnSend.on('click', e => {
+            console.log(this.el.inputText.innerHTML);
+        });
+
+        //Botão dos emojis
+
+        this.el.btnEmojis.on('click', e => {
+            this.el.panelEmojis.toggleClass('open');
+        });
+        // escolhe os emojis
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji=>{
+            emoji.on('click', e=>{
+                let img = this.el.imgEmojiDefault.cloneNode();
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name =>{
+                    img.classList.add(name);
+                });
+                //Adicionar o emoji em qualquer local do texto
+                let cursor = window.getSelection();
+                if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
+                    this.el.inputText.focus();
+                    cursor = window.getSelection();
+                }
+
+                let range = document.createRange();
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+                frag.appendChild(img);
+                range.insertNode(frag);
+                range.setStartAfter(img);
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+            });
+        });
+
+        
+
+
+    }
+
+    //Tempo do microphone
+    startRecordMicrophoneTime() {
+        let start = Date.now();
+        this._recordMicrophoneInterval = setInterval(() => {
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+        }, 100);
+    }
+
+
+    //Fecha o painel de microphone
+    closeRecordMicrophone() {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
+    }
+    //Fecha os paineis principais
+    closeAllMainPanel() {
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
     }
     closeMenuAttach(e) {
         document.removeEventListener('click', this.closeMenuAttach);
