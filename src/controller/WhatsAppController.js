8f593054eed538f3e178ab7c0193b56d57c8eb95
alteon2020 <alteonalves@gmail.com>
2021-06-28 22:21:51 -0300

    aula03

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -66,9 +66,10 @@ class WhatsAppController {
      *
      */
     initEvents() {
+
         //Abre o painel de edição de foto
         this.el.myPhoto.on('click', e => {
-            
+
             this.closeAllLeftPanel();
             this.el.panelEditProfile.show();
             setTimeout(() => {
@@ -76,13 +77,15 @@ class WhatsAppController {
             }, 300);
 
         });
+
         //Fecha o painel de adição de foto
         this.el.btnClosePanelEditProfile.on('click', e => {
             this.el.panelEditProfile.removeClass('open');
         });
+
         //Abre o painel de novo contato
         this.el.btnNewContact.on('click', e => {
-            
+
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
             setTimeout(() => {
@@ -90,12 +93,51 @@ class WhatsAppController {
             }, 300);
 
         });
+
         //Fecha o painel de novo contato
         this.el.btnClosePanelAddContact.on('click', e => {
             this.el.panelAddContact.removeClass('open');
         });
-    }
 
+        // clique e opções da conversa
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+            item.on('click', e => {
+                this.el.home.hide();
+                this.el.main.css({
+                    display: 'flex'
+                });
+            });
+        });
+
+        // Evento do botão de anexo de arquivos diversos
+        this.el.btnAttach.on('click', e => {
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+        });
+
+        //Evento para anexar foto
+        this.el.btnAttachPhoto.on('click', e => {
+            console.log('photo');
+        });
+        //Evento para anexar camera
+        this.el.btnAttachCamera.on('click', e => {
+            console.log('Camera');
+        });
+        //Evento para anexar foto
+        this.el.btnAttachDocument.on('click', e => {
+            console.log('Document');
+        });
+        //Evento para anexar foto
+        this.el.btnAttachContact.on('click', e => {
+            console.log('contact');
+        });
+    }
+    closeMenuAttach(e) {
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+    }
+    //Fecha os paineis para não sobrepor um ao outro
     closeAllLeftPanel() {
         this.el.panelAddContact.hide();
         this.el.panelEditProfile.hide();
