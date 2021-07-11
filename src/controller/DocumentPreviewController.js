32f728d65d7f82af83dee6650f30649e929c2895
alteon2020 <alteonalves@gmail.com>
2021-07-11 20:00:30 -0300

    Há um problema

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -1,17 +1,23 @@
+const pdfjsLib = require('pdfjs-dist');
+const path = require('path');
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.resolve(__dirname, '../../dist/pdf.worker.bundle.js');
+
 export class DocumentPreviewController {
-    
-    constructor(file){
+
+    constructor(file) {
         this._file = file;
     }
 
-    getPreviewData(){
-        return new Promise((s, f)=>{
+    getPreviewData() {
+        return new Promise((s, f) => {
+            let reader = new FileReader();
             switch (this._file.type) {
                 case 'image/png':
                 case 'image/jpeg':
                 case 'image/jpg':
                 case 'image/gif':
-                    let reader = new FileReader();
+
                     reader.onload = e => {
                         s({
                             src: reader.result,
@@ -24,7 +30,38 @@ export class DocumentPreviewController {
                     reader.readAsDataURL(this._file);
                     break;
                 case 'application/pdf':
-                    
+
+                    reader.onload = e => {
+                        pdfjsLib.getDocument(new Uint8Array(reader.result)).then(pdf => {
+                            pdf.getPage(1).then(page =>{
+                                let viewport = page.getViewPort(1);
+                                let canvas = document.createElement('canvas');
+                                let canvasContext = canvas.getContext('2d');
+
+                                canvas.width = viewport.width;
+                                canvas.height = viewport.height;
+
+                                page.render({
+                                    canvasContext,
+                                    viewport,
+                                }).then(()=>{
+                                    console.log('Teste', pdf.numPages)
+                                    let _s = (pdf.numPages > 1) ? 's' : '';
+                                    s({
+                                        src: canvas.toDataURL('image/jpeg'),
+                                        info: `${pdf.numPages} página${_s}`,
+                                    });
+                                }).catch(err=>{
+                                    f(err);
+                                });
+                            }).catch(err=>{
+                                f(err);
+                            });
+                        }).catch(err => {
+                            f(err);
+                        });
+                    }
+                    reader.readAsArrayBuffer(this._file);
                     break;
                 default:
                     f(e);
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -191,6 +191,10 @@ export class WhatsAppController {
         //Capturando o arquivo
         this.el.inputDocument.on('change', e => {
             if (this.el.inputDocument.files.length) {
+
+                this.el.panelDocumentPreview.css({
+                    'height': '1%'
+                });
                 let file = this.el.inputDocument.files[0];
                 this._documentPreviewController = new DocumentPreviewController(file);
 
@@ -200,7 +204,16 @@ export class WhatsAppController {
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
+
+                    console.log(file.type);
+
                 }).catch(err => {
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    });
                     switch (file.type) {
                         case 'application/vnd.ms-excel':
                         case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
