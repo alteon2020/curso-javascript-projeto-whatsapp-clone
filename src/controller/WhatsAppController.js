7e41b0b7c373e79dc1aa166aa2a02c53db47a0dd
alteon2020 <alteonalves@gmail.com>
2022-03-01 18:39:05 -0300

    Setando o cursor no final da conversa

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -160,26 +160,37 @@ export class WhatsAppController {
             display: 'flex'
         });
 
+        this.el.panelMessagesContainer.innerHTML = '';
         Message.getRef(this._contactActive.chatId)
         .orderBy('timeStamp')
         .onSnapshot(docs => {
-            this.el.panelMessagesContainer.innerHTML = '';
-
+            let scrollTop = this.el.panelMessagesContainer.scrollTop;
+            let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - 
+            this.el.panelMessagesContainer.offsetHeight);
+            let autoscroll = (scrollTop >= scrollTopMax);
             docs.forEach(doc => {
                 let data = doc.data();
                 data.id = doc.id;
-                if(!this.el.panelMessagesContainer.querySelector('#_'+data.id)){
-                    let me = (data.from === this._user.email);
+                if (!this.el.panelMessagesContainer.querySelector('#_'+data.id)) {
+                    
                     let message = new Message();
                     
                     message.fromJSON(data);
                     
+                    let me = (data.from === this._user.email); 
+                   
                     let view = message.getviewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
+                    
                 }                
             });
-        })
+            if (autoscroll) {
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+            } else {
+              this.el.panelMessagesContainer.scrollTop = scrollTop;
+            }
+        });
     }
 
     /*
